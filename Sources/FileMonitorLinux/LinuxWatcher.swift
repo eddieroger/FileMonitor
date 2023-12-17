0a02e78764cd0c95968bc56588704c6e9b83ac6d
Eddie Roger <eddieroger@gmail.com>
2023-12-16 22:50:53 -0500

    initial directory event support

--- Sources/FileMonitorLinux/LinuxWatcher.swift
+++ Sources/FileMonitorLinux/LinuxWatcher.swift
@@ -25,29 +25,39 @@ public struct LinuxWatcher: WatcherProtocol {
             print("Mask: 0x\(String(format: "%08x", fsEvent.mask))")
             guard let url = URL(string: self.path.path + "/" + fsEvent.name) else { return }
 
-            // Ignore directory changes
-            if fsEvent.mask & InotifyEventMask.inIsDir.rawValue > 0 { return }
-
             var urlEvent: FileChangeEvent? = nil
+            var inIsDir: Bool = false 
+
+            // Ignore directory changes
+            if fsEvent.mask & InotifyEventMask.inIsDir.rawValue > 0 { 
+                inIsDir = true
+             }
+
 
             // File was changed
             if fsEvent.mask & InotifyEventMask.inModify.rawValue > 0
                 || fsEvent.mask & InotifyEventMask.inMoveSelf.rawValue > 0
             {
-                urlEvent = FileChangeEvent.changed(file: url)
+                urlEvent = inIsDir 
+                    ? FileChangeEvent.directoryChanged(file: url) 
+                    : FileChangeEvent.changed(file: url)
             }
             // File added
             else if fsEvent.mask & InotifyEventMask.inCreate.rawValue > 0
                 || fsEvent.mask & InotifyEventMask.inMovedTo.rawValue > 0
             {
-                urlEvent = FileChangeEvent.added(file: url)
+                urlEvent = inIsDir
+                    ? FileChangeEvent.directoryAdded(file: url)
+                    : FileChangeEvent.added(file: url)
             }
             // File removed
             else if fsEvent.mask & InotifyEventMask.inDelete.rawValue > 0
                 || fsEvent.mask & InotifyEventMask.inDeleteSelf.rawValue > 0
                 || fsEvent.mask & InotifyEventMask.inMovedFrom.rawValue > 0
             {
-                urlEvent = FileChangeEvent.deleted(file: url)
+                urlEvent = inIsDir 
+                    ? FileChangeEvent.directoryDeleted(file: url)
+                    : FileChangeEvent.deleted(file: url)
             }
 
             if urlEvent == nil  {
--- Sources/FileMonitorShared/FileChangeEvent.swift
+++ Sources/FileMonitorShared/FileChangeEvent.swift
@@ -9,6 +9,9 @@ public enum FileChangeEvent {
     case added(file: URL)
     case deleted(file: URL)
     case changed(file: URL)
+    case directoryAdded(file: URL)
+    case directoryDeleted(file: URL)
+    case directoryChanged(file: URL)
 
     // Display friendly description of the event
     public var description: String {
@@ -20,6 +23,14 @@ public enum FileChangeEvent {
                 return "Deleted:  \(file.path)"
             case .changed(file: let file):
                 return "Modified: \(file.path)"
+            
+            case .directoryAdded(file: let file):
+                return "Directory Added:    \(file.path)"
+            case .directoryDeleted(file: let file):
+                return "Directory Deleted:  \(file.path)"
+            case .directoryChanged(file: let file):
+                return "Directory Changed:  \(file.path)"
+
             }
         }
     }
